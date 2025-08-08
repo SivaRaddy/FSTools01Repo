7a77b467f44f8829b407302c32e901178ba07d10
Siva <Siva@192.168.1.47>
2025-08-08 10:43:39 +0530

    Initial commit

---
+++ mavenDemoProj_01/src/main/java/com/nit/mavenDemoProj_01/App.java
@@ -0,0 +1,10 @@
+package com.nit.mavenDemoProj_01;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
