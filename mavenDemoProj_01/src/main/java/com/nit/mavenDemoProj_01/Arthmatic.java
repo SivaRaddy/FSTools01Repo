3b32859c46cc1bc3e5f7b3790d08ac8d3d34bf66
Siva <Siva@192.168.1.47>
2025-08-09 18:29:15 +0530

    Commit

--- mavenDemoProj_01/src/main/java/com/nit/mavenDemoProj_01/Arthmatic.java
+++ mavenDemoProj_01/src/main/java/com/nit/mavenDemoProj_01/Arthmatic.java
@@ -4,5 +4,9 @@ public class Arthmatic {
 	public int sum(int x,int y) {
 		return x+y;
 	}
+	//the operations as int as subtraction operations
+	public int sub(int x,int y) {
+		return x-y;
+	}
 
 }
