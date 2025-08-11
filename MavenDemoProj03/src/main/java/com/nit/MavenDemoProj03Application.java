8db5a7e8bff4ad8ea326657a36314e1aa136dcf0
Siva <Siva@192.168.1.43>
2025-08-11 14:37:54 +0530

    tl commit

---
+++ MavenDemoProj03/src/main/java/com/nit/MavenDemoProj03Application.java
@@ -0,0 +1,13 @@
+package com.nit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenDemoProj03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenDemoProj03Application.class, args);
+	}
+
+}
