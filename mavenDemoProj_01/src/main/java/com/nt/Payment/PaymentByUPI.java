ea696304d27f7cc2baa74bc0269102344b161fef
Siva <Siva@192.168.1.43>
2025-08-12 10:13:42 +0530

    commit

--- mavenDemoProj_01/src/main/java/com/nt/Payment/PaymentByUPI.java
+++ mavenDemoProj_01/src/main/java/com/nt/Payment/PaymentByUPI.java
@@ -6,4 +6,8 @@ public class PaymentByUPI {
 		return Amonut+"amount sent by phone pay"+number;
 	}
 
+	public String gpay(long number,double Amonut) {
+		return Amonut+"amount sent by g pay"+number;
+	}
+
 }
